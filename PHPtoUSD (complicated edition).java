e9ddcd8e41f155e6e43141517690beada02161fe
pointlessduffin-21 <70265407+pointlessduffin-21@users.noreply.github.com>
2022-09-06 16:59:06 +0800

    Added a Comment

--- PHPtoUSD (complicated edition).java
+++ PHPtoUSD (complicated edition).java
@@ -1,3 +1,6 @@
+/*This code was a reverse engineering job done by yours truly. It's more complicated than our teacher's reference code due to its use of a manual Currency converter 
+but it gets the job done and it teaches you how to make your code more efficient and easier to use. */
+
 import java.util.Scanner;
 
 public class Exercise1 {
